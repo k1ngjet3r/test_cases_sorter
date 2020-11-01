f397bfca8788fbeb39ca1f89cda0295cc3221420
K1NGJET3R <kingice00@gmail.com>
2020-11-01 10:42:43 +0800

    Update TC_sorter.py
    adding the word matcher function

--- TC_sorter.py
+++ TC_sorter.py
@@ -1,6 +1,18 @@
 from openpyxl import load_workbook
 from openpyxl import Workbook
 
+# Defining the word matcher function
+
+
+def matcher(keywords, sentance):
+    for key in keywords:
+        num_slices = int(len(sentance)) + 1 - int(len(key))
+        for i in range(num_slices):
+            if sentance[i: i + len(key)] == key:
+                return True
+    return False
+
+
 # Load the unsorted cases file
 workbook = load_workbook('cases.xlsx')
 sheet = workbook.active
@@ -31,5 +43,4 @@ navigation = ['navigation', 'go to', 'add stop', 'guidance']
 
 ac = ['a/c', 'temperature', 'climate control', 'defroster', 'air', 'fan']
 
-#determine the cases of the media center
-for 
+# determine the cases of the media center
