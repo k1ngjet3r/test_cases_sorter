53c0ffb2d7be973c189b2d667f8fbd0855588bfa
K1NGJET3R <kingice00@gmail.com>
2020-11-22 21:25:03 +0800

    Update playground.py

--- playground.py
+++ playground.py
@@ -2,14 +2,21 @@ from openpyxl import load_workbook
 from openpyxl import Workbook
 import re
 
-categories = ['flash user', 'multi_user', 'press_button', 'invalid',
-              'Driver_In_Off', 'Driver_In_On', 'Driver_Out_Off', 'Driver_Out_On',
-              'Guest_In_Off', 'Guest_In_On', 'Guest_Out_Off', 'Guest_Out_On']
+flash_user = []
+press_button = []
+invalid = []
+Driver_In_Off = []
+Driver_In_On = []
+Driver_Out_Off = []
+Driver_Out_On = []
+Guest_In_Off = []
+Guest_In_On = []
+Guest_Out_Off = []
+Guest_Out_On = []
 
 # keyword for layer 1
 flash_user = ['flash']
-multi_user = ['multi', 'primary' 'secondary']
-press_button = ['long press', 'short press', 'press "end" key', 'ignition']
+press_button = ['long press', 'short press', 'press "end" key']
 invalid = ['audiobook', 'sxm']
 
 exceptions_names = ['flash_user', 'multi_user', 'press_button', 'invalid']
@@ -18,30 +25,14 @@ exceptions_items = [flash_user, multi_user, press_button, invalid]
 exp_dict = {name: item for name,
             item in zip(exceptions_names, exceptions_items)}
 
-guest = ['guest']
-
 # Layer 2
 sign_out = ['sign out', 'sign-out', 'signout', 'signed out']
+
 offline = ['offline']
 
-# keyword for determining the function-related categories
-navigation = ['navigat', 'go to', 'add stop', 'guidance',
-              'how far', 'take me', 'address', 'traffic']
-
-call_SMS = ['call', 'phone', 'message', 'messages',
-            'reply', 'text', 'sms', 'dial', 'Send', 'dail']
-
-media = ['play', 'pause', 'next', 'previous',
-         'volume', 'music', 'am', 'fm', 'radio', 'news', 'tune', 'tuned', 'plays', 'bluetooth', 'station', 'album', 'podcast', 'pauses', 'media', 'songs', 'playback', 'bt']
-
+guest = ['guest']
 
 ac = ['a/c', 'temperature', 'climate', 'defroster', 'air', 'fan']
-
-function_items = [call_SMS, media, ac, navigation]
-
-function_names = ['call_SMS', 'media', 'ac', 'navigation']
-
-functions = {name: item for name, item in zip(function_names, function_items)}
 
 
 def matcher_slice(keywords, cell_data, index_range):
@@ -64,118 +55,86 @@ def matcher_split(keywords, cell_data, index_range):
 
 
 class Tc_sorter:
-    def __init__(self, cell_data, categories):
+    def __init__(self, cell_data):
         self.cell_data = cell_data
-        self.categories = categories
-        self.sheet = (load_workbook(self.input_name)).active
-        self.wb = Workbook()
-        self.wb.active
-        for category in self.categories:
-            catagory = []
-
-    def sorting_exceptions(self, cell_data, exceptions):
-        wb = self.wb
-        index_range = [1, 2]
-        for name in exceptions:
-            if name != 'press_button':
-                if matcher_split(exceptions[name], cell_data, index_range):
-                    wb[name].append(cell_data)
-            else:
-                if matcher_slice(exceptions[name], cell_data, index_range):
-                    wb[name].append(cell_data)
 
     def phone_type(self, cell_data):
-        if matcher_split(['iphone'], cell_data, [1]):
-            return "iPhone"
-        elif matcher_slice(['android'], cell_data, [1]):
-            return 'Android'
-        elif matcher_split(['iphone'], cell_data, [1]) and matcher_slice(['android'], cell_data, [1]):
-            return 'Both'
+        iphone = ['iphone', 'cp', 'wcp']
+        android = ['android', 'aa', 'waa']
+        if matcher_split(iphone, cell_data, [1, 2, 3]):
+            cell_data.append("iPhone")
+        elif matcher_split(android, cell_data, [1, 2, 3]):
+            cell_data.append('Android')
+        elif matcher_split(iphone, cell_data, [1, 2, 3]) and matcher_split(android, cell_data, [1, 2, 3]):
+            cell_data.append('Both')
         else:
-            return ' '
+            cell_data.append(' ')
 
-    def projection_type(self, cell_data):
-        if matcher_split(['carplay'], cell_data, [1]):
-            return "Apple CarPlay"
-        elif matcher_slice(['android auto', 'waa'], cell_data, [1]):
-            return "Android Auto"
+    def sign_status(self, cell_data):
+        sign_out = ['sign out', 'sign-out', 'signout', 'signed out']
+        if matcher_slice(sign_out, cell_data, [1]):
+            cell_data.append('sign out')
         else:
-            return ' '
+            cell_data.append('sign in')
 
-    def function_determaination(self, cell_data, functions):
-        for name in functions:
-            if name == 'navigation':
-                if matcher_slice(functions[name], cell_data, [2, 3]):
-                    return name
-            else:
-                if matcher_split(functions[name], cell_data, [2, 3]):
-                    return name
-
-    def appending(self, name, cell_data):
-        function = self.function_determaination(cell_data, functions)
-        cell_data.append(function)
-        cell_data.append(self.phone_type(cell_data))
-        projection = self.projection_type(cell_data)
-        cell_data.append(projection)
-        self.wb[name].append(cell_data)
+    def connection(self, cell_data):
+        offline = ['offline']
+        if matcher_split(offline, cell_data, [1]):
+            cell_data.append('offline')
+        else:
+            cell_data.append('online')
 
     def sorting(self):
-        for row in (self.sheet).rows:
-            cell_data = self.cell_data(row)
+        # Filter out the exception cases
+        if matcher_split(flash_user, cell_data, [1, 2]):
+            flash_user.append(cell_data)
 
-            # Filter out the exception cases
-            if matcher_split(flash_user, cell_data, [1, 2]):
-                flash_user.append(cell_data)
+        elif matcher_split(invalid, cell_data, [1, 2]):
+            invalid.append(cell_data)
 
-            elif matcher_split(multi_user, cell_data, [1, 2]):
-                multi_user.append(cell_data)
+        elif matcher_slice('press_button', cell_data, [1, 2]):
+            press_button.append(cell_data)
 
-            elif matcher_split(invalid, cell_data, [1, 2]):
-                invalid.append(cell_data)
+        else:
+            # determine the Driver as user
+            if matcher_split(guest, cell_data, [1]) == False:
+                # determine sign-in
+                if matcher_split(sign_out, cell_data, [1]) == False:
+                    # determine online
+                    if matcher_split(offline, cell_data, [1]) == False:
+                        Driver_In_On.append(cell_data)
+                    # determine offline
+                    elif matcher_split(offline, cell_data, [1]):
+                        Driver_In_Off.append(cell_data)
 
-            elif matcher_slice('press_button', cell_data, [1, 2]):
-                press_button.append(cell_data)
-
-            else:
-                # determine the Driver as user
-                if matcher_split(guest, cell_data, [1]) == False:
-                    # determine sign-in
-                    if matcher_split(sign_out, cell_data, [1]) == False:
-                        # determine online
-                        if matcher_split(offline, cell_data, [1]) == False:
-                            Driver_In_On.append(cell_data)
-                        # determine offline
-                        elif matcher_split(offline, cell_data, [1]):
-                            Driver_In_Off.append(cell_data)
-
-                    # determine sign-out
-                    elif matcher_split(sign_out, cell_data, [1]):
-                        # determine online
-                        if matcher_split(offline, cell_data, [1]) == False:
-                            Driver_Out_On.append(cell_data)
-                        # determine offline
-                        elif matcher_split(offline, cell_data, [1]):
-                            Driver_Out_Off.append(cell_data)
+                # determine sign-out
+                elif matcher_split(sign_out, cell_data, [1]):
+                    # determine online
+                    if matcher_split(offline, cell_data, [1]) == False:
+                        Driver_Out_On.append(cell_data)
+                    # determine offline
+                    elif matcher_split(offline, cell_data, [1]):
+                        Driver_Out_Off.append(cell_data)
 
                 # determine the Guest as user
-                elif matcher_split(guest, cell_data, [1]):
-                    # determine sign-in
-                    if matcher_split(sign_out, cell_data, [1]) == False:
-                        # determine online
-                        if matcher_split(offline, cell_data, [1]) == False:
-                            Guest_In_On.append(cell_data)
-                        # determine offline
-                        elif matcher_split(offline, cell_data, [1]):
-                            Guest_In_Off.append(cell_data)
+            elif matcher_split(guest, cell_data, [1]):
+                # determine sign-in
+                if matcher_split(sign_out, cell_data, [1]) == False:
+                    # determine online
+                    if matcher_split(offline, cell_data, [1]) == False:
+                        Guest_In_On.append(cell_data)
+                    # determine offline
+                    elif matcher_split(offline, cell_data, [1]):
+                        Guest_In_Off.append(cell_data)
 
-                    # determine sign-out
-                    elif matcher_split(sign_out, cell_data, [1]):
-                        # determine online
-                        if matcher_split(offline, cell_data, [1]) == False:
-                            Guest_Out_On.append(cell_data)
-                        # determine offline
-                        elif matcher_split(offline, cell_data, [1]):
-                            Guest_Out_Off.append(cell_data)
+                # determine sign-out
+                elif matcher_split(sign_out, cell_data, [1]):
+                    # determine online
+                    if matcher_split(offline, cell_data, [1]) == False:
+                        Guest_Out_On.append(cell_data)
+                    # determine offline
+                    elif matcher_split(offline, cell_data, [1]):
+                        Guest_Out_Off.append(cell_data)
 
 
 row_1 = ['TC_Android_Auto_0001_Wireless',
