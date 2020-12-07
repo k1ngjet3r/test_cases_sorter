416f91e13de74e651a0d595d250626934a25b97f
K1NGJET3R <kingice00@gmail.com>
2020-12-07 09:58:28 +0800

    12/07 update
    1. Create the difficult_list_gen.py to pull difficult case ID into a list
    2. add the difficult cases list generated by the difficult_list_gen.py to the MY_22_sorter.py

---
+++ difficult_list_gen.py
@@ -0,0 +1,21 @@
+import re
+from openpyxl import load_workbook
+from openpyxl import Workbook
+
+difficult_test = load_workbook('MY22 Focus test.xlsx')
+sheet = difficult_test.active
+difficult_list = []
+
+for row in sheet.iter_rows(max_col=4, values_only=True):
+    cells = []
+    for cell in row:
+        if cell is None:
+            cells.append('-')
+        else:
+            cells.append(cell)
+    if cells[-1] != '-':
+        difficult_list.append(cells[-1])
+    elif cells[-1] == '-':
+        break
+
+print(len(difficult_list[1:]))
