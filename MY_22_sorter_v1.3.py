2db88e799dd806b4c290fc4a778be8b60e8a2729
Jeter Lin <43425262+k1ngjet3r@users.noreply.github.com>
2020-12-28 18:53:19 +0800

    Update MY_22_sorter_v1.3.py
    Updated bench only keywords

--- MY_22_sorter_v1.3.py
+++ MY_22_sorter_v1.3.py
@@ -138,7 +138,7 @@ class Tc_sorter:
 
     def bench_only(self, cell_data):
         press_button = ['long press', 'short press', 'press "end" key']
-        cluster = ['cluster', 'swc', 'ipc']
+        cluster = ['cluster', 'swc', 'ipc', 'clustor']
         speed_limit = ['speed limit']
         expection = ['short press Power key', 'Long press Power button',
                      'DLM', 'short press selection buttion on the rotary wheel']
