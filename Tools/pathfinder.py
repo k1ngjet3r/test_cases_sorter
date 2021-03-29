d16a6bbdaa7bd70fc53fac97cfd2455605209b7c
k1ngjet3r <jeter.lin1208@gmail.com>
2021-03-29 11:22:22 +0800

    W14 usage

--- Tools/pathfinder.py
+++ Tools/pathfinder.py
@@ -5,24 +5,27 @@ from openpyxl import load_workbook
 
 
 def path_finder(filename, search_path):
-    print('finding file {}...'.format(filename))
+    # print('finding file {}...'.format(filename))
     for root, direction, files in os.walk(search_path):
-        if filename in files:
+        if filename.lower() in files:
             # print('Found!')
-            print(root+'\\'+filename)
+            # print(root+'\\'+filename)
             return root+'\\'+filename
 
 def move_to_destination(target):
-    destination = 'C:\\Users\\GM-PC-03\\Desktop\\temp\\'
+    destination = 'C:\\Users\\GM-PC-03\\Desktop\\phase_1\\gas_user_1\\'
     copy2(target, destination)
 
 
 
 if __name__ == '__main__':
-    case_list = load_workbook('Cases_comparison.xlsx')['Both_automated']
+    case_list = load_workbook('MY22_intersect_scripts.xlsx')['gas_user_1']
     search_path = 'C:\\Users\\GM-PC-03\\Desktop\\Automation\\src\\1080p_tt\\scripts'
-
-    for case in case_list.iter_rows(max_col=1, values_only=True):
-        case_name = case[0] + '.xml'
+    current = 1
+    for case in case_list.iter_rows(max_col=2, values_only=True):
+        print('finding case number: {}'.format(current))
+        current += 1
+        case_name = case[1] + '.xml'
         if path_finder(case_name, search_path):
+            print('found')
             move_to_destination(path_finder(case_name, search_path))
