2a330140b66dfedb15658eec882530eac5884946
k1ngjet3r <jeter.lin1208@gmail.com>
2021-04-07 14:43:44 +0800

    Update pathfinder.py

--- Tools/pathfinder.py
+++ Tools/pathfinder.py
@@ -10,13 +10,13 @@ def path_finder(filename, search_path):
             return root+'\\'+filename
 
 def move_to_destination(target):
-    destination = 'C:\\Users\\GM-PC-03\\Desktop\\phase_1\\ignition_package_2\\'
+    destination = 'C:\\Users\\GM-PC-03\\Desktop\\phase_1\\gas_user_1\\'
     copy2(target, destination)
 
 
 if __name__ == '__main__':
-    case_list = load_workbook('MY22_intersect_scripts.xlsx')['Ignition_package_2']
-    search_path = 'C:\\Users\\GM-PC-03\\Desktop\\Automation\\src\\1080p_tt\\scripts'
+    case_list = load_workbook('MY22_intersect_scripts.xlsx')['gas_user_1']
+    search_path = 'C:\\Users\\GM-PC-03\\Desktop\\Automation\\src\\1080p_tt\\scripts\\bj'
     current = 1
     for case in case_list.iter_rows(max_col=2, values_only=True):
         print('finding case number: {}'.format(current))
