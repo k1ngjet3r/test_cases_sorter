77cb557c419d61945dc4c6c94530d94cf13ff4cf
k1ngjet3r <jeter.lin1208@gmail.com>
2021-04-15 19:27:27 +0800

    adding counter function

--- Tools/eli.py
+++ Tools/eli.py
@@ -3,8 +3,8 @@ from openpyxl import Workbook, load_workbook
 
 class eliminator:
     def __init__(self, partial_list, full_list, tcid_only=True):
-        self.partial_list = load_workbook(partial_list).active
-        self.full_list = load_workbook(full_list).active
+        self.partial_list = load_workbook(partial_list)['Main']
+        self.full_list = load_workbook(full_list)['Production']
         if tcid_only == True:
             self.max_column = 1
         else:
@@ -27,3 +27,25 @@ class eliminator:
 
         wb.save('cases.xlsx')
 
+    def differentiator_counter(self):
+        current = 1
+        matched = 0
+        not_matched = 0
+        partial_tcid = [row[0] for row in self.partial_list.iter_rows(
+            max_col=1, values_only=True)]
+
+        for row in self.full_list.iter_rows(max_col=1, values_only=True):
+            print('iterate case number {}'.format(current))
+            try:
+                current +=1
+                if row[0] in partial_tcid:
+                    matched += 1
+                else:
+                    not_matched += 1
+            except:
+                break
+        print('Matched: {}'.format(matched))
+        print('Not Matched: {}'.format(not_matched))
+
+if __name__ == '__main__':
+    eliminator('MY22_Scope.xlsx', 'MY22_Scope.xlsx').differentiator_counter()
